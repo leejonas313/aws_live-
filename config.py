8499cb87a2f49441f8dddce5895511a1277027b8
leejonas313 <163797521+leejonas313@users.noreply.github.com>
2025-12-01 11:06:38 +0800

    Update S3 bucket name in config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "admin123"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "leekahhoe-employee"
 customregion = "us-east-1"
 
